1446f52acfb99f90289026413f9a997027502f79
唐道勇 <matias@tangdaoyongdeMacBook-Pro.local>
2024-07-16 17:33:11 +0800

    feat: - 删除废弃文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,25 +2,19 @@
  * @Author: matiastang
  * @Date: 2021-12-28 19:31:46
  * @LastEditors: matiastang
- * @LastEditTime: 2022-11-15 15:36:18
+ * @LastEditTime: 2024-07-16 17:32:42
  * @FilePath: /mt-storage/src/router/index.ts
  * @Description: 路由
  */
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 // web
 import Index from '@/views/index.vue'
-import LocalTest from '@/views/localTest.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'index',
         component: Index,
-    },
-    {
-        path: '/local',
-        name: 'LocalTest',
-        component: LocalTest,
     },
     {
         path: '/:pathMatch(.*)*',
